671b86b0cfb9b57be7ac1580feb5ed3e2aee1cc7
munaga-venkatesh <munagavenkatesh02@gmail.com>
2026-05-17 19:15:22 +0530

    Initial Commit

---
+++ DiceView/DiceViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceViewApp.swift
+//  DiceView
+//
+//  Created by Venkatesh Munaga on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct DiceViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
